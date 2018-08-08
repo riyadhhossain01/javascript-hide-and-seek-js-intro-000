b1c4e81434ba6697d59cb57bf4f8891523524b3c
riyadh hossain <riyadh_h@ymail.com>
2018-08-08 20:29:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+// function getFirstSelector(selector){
+//   return document.querySelector(selector);
+// }
+// function nestedTarget(){
+//   return document.querySelector('#nested .target');
+// }
+// function deepestChild(){
+//
+// }
+// function increaseRankBy(n){
+//   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+//
+//   for (let i = 0; i < lis.length; i++) {
+//     rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+//   }
+// }
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
 
+}
+function increaseRankBy(n){
+  const li = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(var i = 0; i< li.length; i++){
+    li[i].innerHTML = parseInt(li[i].innerHTML) + n
+  }
+}
+function deepestChild(){
+return document.querySelector('#grand-node div div div div')
+}
